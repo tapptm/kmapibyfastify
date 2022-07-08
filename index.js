73fab9156eecbcd3e09eca5b1546c66647d4f82d
tapptm <phuwadet00123@gmail.com>
2022-07-08 11:42:06 +0700

    edit port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const routes = require("./app");
 
 fastify.register(routes, { prefix: "/api/v2" });
 
-fastify.listen({ port: 3000 }, (err) => {
+fastify.listen({ port: 3007 }, (err) => {
   if (err) throw err;
   console.log(`server listening on ${fastify.server.address().port}`);
 });
